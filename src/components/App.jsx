5ac42fb02966729034313c15b68bf0f462b4897b
Kyle <sully8437@gmail.com>
2017-01-09 10:22:53 -0600

    Finished the template

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                MY APP
+            </div>
+        )
+    }
+}
+
+export default App
